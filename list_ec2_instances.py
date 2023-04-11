d8f41b61a09b25c4583b246a727cf48175c8d732
wiehahne <eugene.wiehahn@gmail.com>
2023-04-10 21:24:27 -0500

    Included region

--- list_ec2_instances.py
+++ list_ec2_instances.py
@@ -18,21 +18,28 @@ def get_instance_details(region):
                 name = tag['Value']
                 break
         instance_details.append({
+            'Region': region,
             'Instance ID': instance.id,
             'Name': name,
             'Type': instance.instance_type,
             'vCPUs': instance.cpu_options['CoreCount'] * instance.cpu_options['ThreadsPerCore'],
-            #'Memory': instance.memory, # Please note that this attribute is not available in Boto3. You will need to map instance types to their memory sizes manually or use another API/library.
+            'Memory': instance.memory, # Please note that this attribute is not available in Boto3. You will need to map instance types to their memory sizes manually or use another API/library.
         })
     return instance_details
 
 def write_to_csv(instance_details):
     with open('ec2_instances.csv', mode='w', newline='') as csvfile:
-        fieldnames = ['Instance ID', 'Name', 'Type', 'vCPUs', 'Memory']
+        fieldnames = ['Region', 'Instance ID', 'Name', 'Type', 'vCPUs', 'Memory']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for instance_detail in instance_details:
             writer.writerow(instance_detail)
+
+def upload_to_s3():
+    s3_client = boto3.client('s3')
+    bucket_name = 'your-bucket-name'
+    s3_key = 'your-prefix/ec2_instances.csv'
+    s3_client.upload_file('ec2_instances.csv', bucket_name, s3_key)
 
 def main():
     all_regions = get_all_regions()
@@ -43,6 +50,7 @@ def main():
         all_instance_details.extend(instance_details)
 
     write_to_csv(all_instance_details)
+    upload_to_s3()
 
 if __name__ == '__main__':
     main()
